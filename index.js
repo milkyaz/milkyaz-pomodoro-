d6858f04c1018992e041d893e21c65eecc90f61a
Daniil Zaburunov <zaburunov.2000@mail.ru>
2025-11-17 23:48:58 +0400

    main: make the timer start

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const pomodoroElem = document.querySelector('[data-js-pomodoro]');
 pomodoroElem.innerHTML = '00:00';
 
 const btnStart = document.querySelector('[data-js-start]');
+
+// чтобы цифры начинали идти
+
+let numPomodoro = 10;
+
+function countDown() {
+    if(numPomodoro >= 0) {
+        pomodoroElem.innerHTML = numPomodoro < 10 ? `00:0${numPomodoro}` : `00:${numPomodoro}`;
+        numPomodoro--;
+        setTimeout(countDown, 1000);
+    }
+}
+
+btnStart.addEventListener('click', countDown);
