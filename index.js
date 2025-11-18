d8dcec5063a7c0e2e0c625b3722e321fe75d61b2
Daniil Zaburunov <zaburunov.2000@mail.ru>
2025-11-18 11:38:01 +0400

    main: create btn stop

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const pomodoroElem = document.querySelector('[data-js-pomodoro]');
-pomodoroElem.innerHTML = '00:00';
+
 
 const btnStart = document.querySelector('[data-js-start]');
+const btnStop = document.querySelector('[data-js-stop]');
 
 // чтобы цифры начинали идти
 
 let numPomodoro = 10;
+let timerId;
+pomodoroElem.innerHTML = `00:${numPomodoro}`;
 
-function countDown() {
+function startTimer() {
     if(numPomodoro >= 0) {
         pomodoroElem.innerHTML = numPomodoro < 10 ? `00:0${numPomodoro}` : `00:${numPomodoro}`;
         numPomodoro--;
-        setTimeout(countDown, 1000);
+        timerId = setTimeout(startTimer, 1000);
     }
 }
 
-btnStart.addEventListener('click', countDown);
+btnStart.addEventListener('click', startTimer);
+btnStop.addEventListener('click', () =>  clearTimeout(timerId))
