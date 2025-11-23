c545aa2202fab76186e615f5e67b646a598a4524
Daniil Zaburunov <zaburunov.2000@mail.ru>
2025-11-23 20:04:05 +0400

    create button reset

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const pomodoroElem = document.querySelector('[data-js-pomodoro]');
 
 
-const btnStart = document.querySelector('[data-js-start]');
-const btnStop = document.querySelector('[data-js-stop]');
+const btnStartElement = document.querySelector('[data-js-start]');
+const btnStopElement = document.querySelector('[data-js-stop]');
+const btnResetElement = document.querySelector('[data-js-reset]');
 
-// чтобы цифры начинали идти
 
 let numPomodoro = 10;
 let timerId;
 pomodoroElem.innerHTML = `00:${numPomodoro}`;
 
 function startTimer() {
+    console.log(numPomodoro)
     if(numPomodoro >= 0) {
         pomodoroElem.innerHTML = numPomodoro < 10 ? `00:0${numPomodoro}` : `00:${numPomodoro}`;
         numPomodoro--;
         timerId = setTimeout(startTimer, 1000);
     }
+
 }
 
-btnStart.addEventListener('click', startTimer);
-btnStop.addEventListener('click', () =>  clearTimeout(timerId))
+function stopTimer() {
+   return clearTimeout(timerId)
+}
+
+function resetTimer() {
+    numPomodoro = 10;
+    pomodoroElem.innerHTML = `00:${numPomodoro}`;
+    stopTimer();
+}
+
+btnStartElement.addEventListener('click', startTimer);
+btnStopElement.addEventListener('click', stopTimer);
+btnResetElement.addEventListener('click', resetTimer);
+
