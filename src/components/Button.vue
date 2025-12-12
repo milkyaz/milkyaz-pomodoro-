8939d3083f9785f6673e66dba2816b341c2ed4bd
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2025-12-12 13:30:43 +0400

    issue-8: add btn-disavled logic

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { useMainStore } from '@/stores/store.ts'
 
 const emit = defineEmits(['click']);
-
+const mainStore = useMainStore();
 const props = defineProps<{
   val?: string;
+  icon?: string;
 }>()
 
 function handleClick() {
@@ -13,7 +16,12 @@ function handleClick() {
 </script>
 
 <template>
-  <div class="flex gap-5">
+  <div v-if="icon" class="flex gap-5 justify-center">
+    <button :class="[mainStore.isDisableBtn ? 'btn-disabled' : '' ]" class="btn btn-outline btn-info btn-md" @click="handleClick">
+      <Icon :icon="icon" width="30" />
+    </button>
+  </div>
+  <div v-else class="flex gap-5">
     <button class="btn btn-outline btn-info btn-lg" @click="handleClick">{{ props.val }}</button>
   </div>
 </template>
--- src/components/TimerDisplay.vue
+++ src/components/TimerDisplay.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-import { useMainStore } from "@/stores/store.ts";
-import Button from "@/components/Button.vue";
+import { useMainStore } from '@/stores/store.ts'
+import Button from '@/components/Button.vue'
 import { Icon } from '@iconify/vue'
 
-const mainStore = useMainStore();
-
+const mainStore = useMainStore()
 </script>
 
 <template>
-  <div class="flex-col justify-center items-center mt-30 ">
+  <div class="flex-col justify-center items-center mt-30">
+    <Button class="mb-10" @click="mainStore.increment" icon="material-symbols:add" />
     <div class="flex justify-center mb-10 text-6xl">
-      <Icon class="rotate-225 absolute left-[565px]"
-            icon="noto:flashlight"
-            width="90"/>
-      <div class="cone-only flex justify-center pl-10 pt-3 text-center relative">{{ mainStore.display }}</div>
+      <Icon class="rotate-225 absolute left-[565px]" icon="noto:flashlight" width="90" />
+      <div class="cone-only flex justify-center pl-10 pt-3 text-center relative">
+        {{ mainStore.display }}
+      </div>
     </div>
+    <Button class="mb-10" @click="mainStore.decrement" icon="material-symbols:remove" />
     <div class="flex gap-5 justify-center items-center w-full">
       <Button @click="mainStore.startTimer" val="START" />
       <Button @click="mainStore.stopTimer" val="STOP" />
@@ -24,10 +25,10 @@ const mainStore = useMainStore();
 </template>
 
 <style scoped>
-
 button {
   width: 100px;
 }
+
 /* Тёплый ламповый свет для Pomodoro */
 /* Просто конус без фона на весь экран */
 .cone-only {
@@ -38,6 +39,6 @@ button {
     rgba(279, 250, 210, 3.8) 0,
     rgba(255, 254, 190, 0.4) 46%,
     transparent 70%
-  )
+  );
 }
 </style>
