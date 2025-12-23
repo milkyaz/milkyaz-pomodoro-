4a9e2a44e16f34dbdc5759ad1991c585cb82234b
Daniil Zaburunov <daniil.zaburunov@fin-frame.ru>
2025-12-23 21:59:25 +0400

    issue-8: update store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,29 +1,72 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useClockSound } from '@/composables/useSound.ts'
 
 export const useMainStore = defineStore('store', () => {
-  const numPomodoro = ref(10);
+  const numPomodoro = ref(0);
   const timerId = ref(0);
   const display = computed(() => {
-    return numPomodoro.value < 10
-      ? `00:0${numPomodoro.value}`
-      : `00:${numPomodoro.value}`;
-  });
+    const totalSeconds = numPomodoro.value;
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
 
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  });
+  const isDisableBtn = ref(false);
+  const { startTicking } = useClockSound();
+
+  function increment() {
+    numPomodoro.value++
+  }
+
+  function addTenNum() {
+    numPomodoro.value = numPomodoro.value + 10;
+  }
+
+  function addMinute() {
+    numPomodoro.value = numPomodoro.value + 60;
+  }
+
+
+  function decrement() {
+    numPomodoro.value--
+    if(numPomodoro.value < 0) {
+      return numPomodoro.value = 0;
+    }
+  }
+
+  function removeTenNum() {
+    numPomodoro.value = numPomodoro.value - 10;
+    if(numPomodoro.value < 0) {
+      return numPomodoro.value = 0;
+    }
+  }
+
+  function removeMinute() {
+    numPomodoro.value = numPomodoro.value - 60;
+    if(numPomodoro.value < 0) {
+      return numPomodoro.value = 0;
+    }
+  }
 
   function startTimer() {
     if (numPomodoro.value > 0) {
+      isDisableBtn.value = true;
+      startTicking();
       numPomodoro.value--;
       timerId.value = setTimeout(startTimer, 1000);
+    } else if (numPomodoro.value <= 0) {
+      isDisableBtn.value = false;
     }
   }
 
   function stopTimer() {
+    isDisableBtn.value = false;
     return clearTimeout(timerId.value)
   }
 
   function resetTimer() {
-    numPomodoro.value = 10;
+    numPomodoro.value = 0;
     stopTimer();
   }
 
@@ -32,8 +75,15 @@ export const useMainStore = defineStore('store', () => {
     display,
     numPomodoro,
     timerId,
+    isDisableBtn,
     startTimer,
     stopTimer,
-    resetTimer
+    resetTimer,
+    increment,
+    decrement,
+    addTenNum,
+    addMinute,
+    removeMinute,
+    removeTenNum,
   };
 })
